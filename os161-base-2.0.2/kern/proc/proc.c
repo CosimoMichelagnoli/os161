265ef173401317cab3e546bb68591ad58b02de78
CosimoMichelagnoli <cosimo.michelagnoli@stud.unifi.it>
2021-09-24 15:47:18 +0200

    init: first commit

---
+++ os161-base-2.0.2/kern/proc/proc.c
@@ -0,0 +1,488 @@
+/*
+ * Copyright (c) 2013
+ *	The President and Fellows of Harvard College.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Process support.
+ *
+ * There is (intentionally) not much here; you will need to add stuff
+ * and maybe change around what's already present.
+ *
+ * p_lock is intended to be held when manipulating the pointers in the
+ * proc structure, not while doing any significant work with the
+ * things they point to. Rearrange this (and/or change it to be a
+ * regular lock) as needed.
+ *
+ * Unless you're implementing multithreaded user processes, the only
+ * process that will have more than one thread is the kernel process.
+ */
+
+#include <types.h>
+#include <spl.h>
+#include <proc.h>
+#include <current.h>
+#include <addrspace.h>
+#include <vnode.h>
+#include <syscall.h>
+
+#if OPT_WAITPID
+#include <synch.h>
+
+#define MAX_PROC 100
+static struct _processTable {
+  int active;           /* initial value 0 */
+  struct proc *proc[MAX_PROC+1]; /* [0] not used. pids are >= 1 */
+  int last_i;           /* index of last allocated pid */
+  struct spinlock lk;	/* Lock for this table */
+} processTable;
+
+#endif
+/*
+ * The process for the kernel; this holds all the kernel-only threads.
+ */
+struct proc *kproc;
+
+/*
+ * G.Cabodi - 2019
+ * Initialize support for pid/waitpid.
+ */
+struct proc *
+proc_search_pid(pid_t pid) {
+#if OPT_WAITPID
+  struct proc *p;
+  KASSERT(pid>=0&&pid<MAX_PROC);
+  p = processTable.proc[pid];
+  KASSERT(p->p_pid==pid);
+  return p;
+#else
+  (void)pid;
+  return NULL;
+#endif
+}
+
+/*
+ * G.Cabodi - 2019
+ * Initialize support for pid/waitpid.
+ */
+static void
+proc_init_waitpid(struct proc *proc, const char *name) {
+#if OPT_WAITPID
+  /* search a free index in table using a circular strategy */
+  int i;
+  spinlock_acquire(&processTable.lk);
+  i = processTable.last_i+1;
+  proc->p_pid = 0;
+  if (i>MAX_PROC) i=1;
+  while (i!=processTable.last_i) {
+    if (processTable.proc[i] == NULL) {
+      processTable.proc[i] = proc;
+      processTable.last_i = i;
+      proc->p_pid = i;
+      break;
+    }
+    i++;
+    if (i>MAX_PROC) i=1;
+  }
+  spinlock_release(&processTable.lk);
+  if (proc->p_pid==0) {
+    panic("too many processes. proc table is full\n");
+  }
+  proc->p_status = 0;
+#if USE_SEMAPHORE_FOR_WAITPID
+  proc->p_sem = sem_create(name, 0);
+#else
+  proc->p_cv = cv_create(name);
+  proc->p_lock = lock_create(name);
+#endif
+#else
+  (void)proc;
+  (void)name;
+#endif
+}
+
+/*
+ * G.Cabodi - 2019
+ * Terminate support for pid/waitpid.
+ */
+static void
+proc_end_waitpid(struct proc *proc) {
+#if OPT_WAITPID
+  /* remove the process from the table */
+  int i;
+  spinlock_acquire(&processTable.lk);
+  i = proc->p_pid;
+  KASSERT(i>0 && i<=MAX_PROC);
+  processTable.proc[i] = NULL;
+  spinlock_release(&processTable.lk);
+
+#if USE_SEMAPHORE_FOR_WAITPID
+  sem_destroy(proc->p_sem);
+#else
+  cv_destroy(proc->p_cv);
+  lock_destroy(proc->p_lock);
+#endif
+#else
+  (void)proc;
+#endif
+}
+
+/*
+ * Create a proc structure.
+ */
+static
+struct proc *
+proc_create(const char *name)
+{
+	struct proc *proc;
+
+	proc = kmalloc(sizeof(*proc));
+	if (proc == NULL) {
+		return NULL;
+	}
+	proc->p_name = kstrdup(name);
+	if (proc->p_name == NULL) {
+		kfree(proc);
+		return NULL;
+	}
+
+	proc->p_numthreads = 0;
+	spinlock_init(&proc->p_lock);
+
+	/* VM fields */
+	proc->p_addrspace = NULL;
+
+	/* VFS fields */
+	proc->p_cwd = NULL;
+
+	proc_init_waitpid(proc,name);
+#if OPT_FILE
+        bzero(proc->fileTable,OPEN_MAX*sizeof(struct openfile *));
+#endif
+	return proc;
+}
+
+/*
+ * Destroy a proc structure.
+ *
+ * Note: nothing currently calls this. Your wait/exit code will
+ * probably want to do so.
+ */
+void
+proc_destroy(struct proc *proc)
+{
+	/*
+	 * You probably want to destroy and null out much of the
+	 * process (particularly the address space) at exit time if
+	 * your wait/exit design calls for the process structure to
+	 * hang around beyond process exit. Some wait/exit designs
+	 * do, some don't.
+	 */
+
+	KASSERT(proc != NULL);
+	KASSERT(proc != kproc);
+
+	/*
+	 * We don't take p_lock in here because we must have the only
+	 * reference to this structure. (Otherwise it would be
+	 * incorrect to destroy it.)
+	 */
+
+	/* VFS fields */
+	if (proc->p_cwd) {
+		VOP_DECREF(proc->p_cwd);
+		proc->p_cwd = NULL;
+	}
+
+	/* VM fields */
+	if (proc->p_addrspace) {
+		/*
+		 * If p is the current process, remove it safely from
+		 * p_addrspace before destroying it. This makes sure
+		 * we don't try to activate the address space while
+		 * it's being destroyed.
+		 *
+		 * Also explicitly deactivate, because setting the
+		 * address space to NULL won't necessarily do that.
+		 *
+		 * (When the address space is NULL, it means the
+		 * process is kernel-only; in that case it is normally
+		 * ok if the MMU and MMU- related data structures
+		 * still refer to the address space of the last
+		 * process that had one. Then you save work if that
+		 * process is the next one to run, which isn't
+		 * uncommon. However, here we're going to destroy the
+		 * address space, so we need to make sure that nothing
+		 * in the VM system still refers to it.)
+		 *
+		 * The call to as_deactivate() must come after we
+		 * clear the address space, or a timer interrupt might
+		 * reactivate the old address space again behind our
+		 * back.
+		 *
+		 * If p is not the current process, still remove it
+		 * from p_addrspace before destroying it as a
+		 * precaution. Note that if p is not the current
+		 * process, in order to be here p must either have
+		 * never run (e.g. cleaning up after fork failed) or
+		 * have finished running and exited. It is quite
+		 * incorrect to destroy the proc structure of some
+		 * random other process while it's still running...
+		 */
+		struct addrspace *as;
+
+		if (proc == curproc) {
+			as = proc_setas(NULL);
+			as_deactivate();
+		}
+		else {
+			as = proc->p_addrspace;
+			proc->p_addrspace = NULL;
+		}
+		as_destroy(as);
+	}
+
+	KASSERT(proc->p_numthreads == 0);
+	spinlock_cleanup(&proc->p_lock);
+
+	proc_end_waitpid(proc);
+
+	kfree(proc->p_name);
+	kfree(proc);
+}
+
+/*
+ * Create the process structure for the kernel.
+ */
+void
+proc_bootstrap(void)
+{
+	kproc = proc_create("[kernel]");
+	if (kproc == NULL) {
+		panic("proc_create for kproc failed\n");
+	}
+#if OPT_WAITPID
+	spinlock_init(&processTable.lk);
+	/* kernel process is not registered in the table */
+	processTable.active = 1;
+#endif
+}
+
+
+/*
+ * Create a fresh proc for use by runprogram.
+ *
+ * It will have no address space and will inherit the current
+ * process's (that is, the kernel menu's) current directory.
+ */
+struct proc *
+proc_create_runprogram(const char *name)
+{
+	struct proc *newproc;
+
+	newproc = proc_create(name);
+	if (newproc == NULL) {
+		return NULL;
+	}
+
+	/* VM fields */
+
+	newproc->p_addrspace = NULL;
+
+	/* VFS fields */
+
+	/*
+	 * Lock the current process to copy its current directory.
+	 * (We don't need to lock the new process, though, as we have
+	 * the only reference to it.)
+	 */
+	spinlock_acquire(&curproc->p_lock);
+	if (curproc->p_cwd != NULL) {
+		VOP_INCREF(curproc->p_cwd);
+		newproc->p_cwd = curproc->p_cwd;
+	}
+	spinlock_release(&curproc->p_lock);
+
+	return newproc;
+}
+
+/*
+ * Add a thread to a process. Either the thread or the process might
+ * or might not be current.
+ *
+ * Turn off interrupts on the local cpu while changing t_proc, in
+ * case it's current, to protect against the as_activate call in
+ * the timer interrupt context switch, and any other implicit uses
+ * of "curproc".
+ */
+int
+proc_addthread(struct proc *proc, struct thread *t)
+{
+	int spl;
+
+	KASSERT(t->t_proc == NULL);
+
+	spinlock_acquire(&proc->p_lock);
+	proc->p_numthreads++;
+	spinlock_release(&proc->p_lock);
+
+	spl = splhigh();
+	t->t_proc = proc;
+	splx(spl);
+
+	return 0;
+}
+
+/*
+ * Remove a thread from its process. Either the thread or the process
+ * might or might not be current.
+ *
+ * Turn off interrupts on the local cpu while changing t_proc, in
+ * case it's current, to protect against the as_activate call in
+ * the timer interrupt context switch, and any other implicit uses
+ * of "curproc".
+ */
+void
+proc_remthread(struct thread *t)
+{
+	struct proc *proc;
+	int spl;
+
+	proc = t->t_proc;
+	KASSERT(proc != NULL);
+
+	spinlock_acquire(&proc->p_lock);
+	KASSERT(proc->p_numthreads > 0);
+	proc->p_numthreads--;
+	spinlock_release(&proc->p_lock);
+
+	spl = splhigh();
+	t->t_proc = NULL;
+	splx(spl);
+}
+
+/*
+ * Fetch the address space of (the current) process.
+ *
+ * Caution: address spaces aren't refcounted. If you implement
+ * multithreaded processes, make sure to set up a refcount scheme or
+ * some other method to make this safe. Otherwise the returned address
+ * space might disappear under you.
+ */
+struct addrspace *
+proc_getas(void)
+{
+	struct addrspace *as;
+	struct proc *proc = curproc;
+
+	if (proc == NULL) {
+		return NULL;
+	}
+
+	spinlock_acquire(&proc->p_lock);
+	as = proc->p_addrspace;
+	spinlock_release(&proc->p_lock);
+	return as;
+}
+
+/*
+ * Change the address space of (the current) process. Return the old
+ * one for later restoration or disposal.
+ */
+struct addrspace *
+proc_setas(struct addrspace *newas)
+{
+	struct addrspace *oldas;
+	struct proc *proc = curproc;
+
+	KASSERT(proc != NULL);
+
+	spinlock_acquire(&proc->p_lock);
+	oldas = proc->p_addrspace;
+	proc->p_addrspace = newas;
+	spinlock_release(&proc->p_lock);
+	return oldas;
+}
+
+
+
+        /* G.Cabodi - 2019 - support for waitpid */
+int 
+proc_wait(struct proc *proc)
+{
+#if OPT_WAITPID
+        int return_status;
+        /* NULL and kernel proc forbidden */
+	KASSERT(proc != NULL);
+	KASSERT(proc != kproc);
+
+        /* wait on semaphore or condition variable */ 
+#if USE_SEMAPHORE_FOR_WAITPID
+        P(proc->p_sem);
+#else
+        lock_acquire(proc->p_lock);
+        cv_wait(proc->p_cv);
+        lock_release(proc->p_lock);
+#endif
+        return_status = proc->p_status;
+        proc_destroy(proc);
+        return return_status;
+#else
+        /* this doesn't synchronize */ 
+        (void)proc;
+        return 0;
+#endif
+}
+
+
+/* G.Cabodi - 2019 - support for waitpid */
+void
+proc_signal_end(struct proc *proc)
+{
+#if USE_SEMAPHORE_FOR_WAITPID
+      V(proc->p_sem);
+#else
+      lock_acquire(proc->p_lock);
+      cv_signal(proc->p_cv);
+      lock_release(proc->p_lock);
+#endif
+}
+
+#if OPT_FILE
+void 
+proc_file_table_copy(struct proc *psrc, struct proc *pdest) {
+  int fd;
+  for (fd=0; fd<OPEN_MAX; fd++) {
+    struct openfile *of = psrc->fileTable[fd];
+    pdest->fileTable[fd] = of;
+    if (of != NULL) {
+      /* incr reference count */
+      openfileIncrRefCount(of);
+    }
+  }
+}
+#endif
---
+++ os161-base-2.0.2/kern/thread/synch.c
@@ -0,0 +1,384 @@
+/*
+ * Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2008, 2009
+ *	The President and Fellows of Harvard College.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Synchronization primitives.
+ * The specifications of the functions are in synch.h.
+ */
+
+#include <types.h>
+#include <lib.h>
+#include <spinlock.h>
+#include <wchan.h>
+#include <thread.h>
+#include <current.h>
+#include <synch.h>
+
+////////////////////////////////////////////////////////////
+//
+// Semaphore.
+
+struct semaphore *
+sem_create(const char *name, unsigned initial_count)
+{
+        struct semaphore *sem;
+
+        sem = kmalloc(sizeof(*sem));
+        if (sem == NULL) {
+                return NULL;
+        }
+
+        sem->sem_name = kstrdup(name);
+        if (sem->sem_name == NULL) {
+                kfree(sem);
+                return NULL;
+        }
+
+	sem->sem_wchan = wchan_create(sem->sem_name);
+	if (sem->sem_wchan == NULL) {
+		kfree(sem->sem_name);
+		kfree(sem);
+		return NULL;
+	}
+
+	spinlock_init(&sem->sem_lock);
+        sem->sem_count = initial_count;
+
+        return sem;
+}
+
+void
+sem_destroy(struct semaphore *sem)
+{
+        KASSERT(sem != NULL);
+
+	/* wchan_cleanup will assert if anyone's waiting on it */
+	spinlock_cleanup(&sem->sem_lock);
+	wchan_destroy(sem->sem_wchan);
+        kfree(sem->sem_name);
+        kfree(sem);
+}
+
+void
+P(struct semaphore *sem)
+{
+        KASSERT(sem != NULL);
+
+        /*
+         * May not block in an interrupt handler.
+         *
+         * For robustness, always check, even if we can actually
+         * complete the P without blocking.
+         */
+        KASSERT(curthread->t_in_interrupt == false);
+
+	/* Use the semaphore spinlock to protect the wchan as well. */
+	spinlock_acquire(&sem->sem_lock);
+        while (sem->sem_count == 0) {
+		/*
+		 *
+		 * Note that we don't maintain strict FIFO ordering of
+		 * threads going through the semaphore; that is, we
+		 * might "get" it on the first try even if other
+		 * threads are waiting. Apparently according to some
+		 * textbooks semaphores must for some reason have
+		 * strict ordering. Too bad. :-)
+		 *
+		 * Exercise: how would you implement strict FIFO
+		 * ordering?
+		 */
+		wchan_sleep(sem->sem_wchan, &sem->sem_lock);
+        }
+        KASSERT(sem->sem_count > 0);
+        sem->sem_count--;
+	spinlock_release(&sem->sem_lock);
+}
+
+void
+V(struct semaphore *sem)
+{
+        KASSERT(sem != NULL);
+
+	spinlock_acquire(&sem->sem_lock);
+
+        sem->sem_count++;
+        KASSERT(sem->sem_count > 0);
+	wchan_wakeone(sem->sem_wchan, &sem->sem_lock);
+
+	spinlock_release(&sem->sem_lock);
+}
+
+////////////////////////////////////////////////////////////
+//
+// Lock.
+
+struct lock *
+lock_create(const char *name)
+{
+        struct lock *lock;
+
+        lock = kmalloc(sizeof(*lock));
+        if (lock == NULL) {
+                return NULL;
+        }
+
+        lock->lk_name = kstrdup(name);
+        if (lock->lk_name == NULL) {
+                kfree(lock);
+                return NULL;
+        }
+
+        // add stuff here as needed
+
+#if OPT_SYNCH
+#if USE_SEMAPHORE_FOR_LOCK
+        lock->lk_sem = sem_create(lock->lk_name,1);
+	if (lock->lk_sem == NULL) {
+#else
+	lock->lk_wchan = wchan_create(lock->lk_name);
+	if (lock->lk_wchan == NULL) {
+#endif
+	  kfree(lock->lk_name);
+	  kfree(lock);
+	  return NULL;
+	}
+	lock->lk_owner = NULL;
+	spinlock_init(&lock->lk_lock);
+#endif	
+        return lock;
+}
+
+void
+lock_destroy(struct lock *lock)
+{
+        KASSERT(lock != NULL);
+
+        // add stuff here as needed
+#if OPT_SYNCH
+	spinlock_cleanup(&lock->lk_lock);
+#if USE_SEMAPHORE_FOR_LOCK
+        sem_destroy(lock->lk_sem);
+#else
+	wchan_destroy(lock->lk_wchan);
+#endif
+#endif
+        kfree(lock->lk_name);
+        kfree(lock);
+}
+
+void
+lock_acquire(struct lock *lock)
+{
+        // Write this
+#if OPT_SYNCH
+        KASSERT(lock != NULL);
+	if (lock_do_i_hold(lock)) {
+	  kprintf("AAACKK!\n");
+	}
+	KASSERT(!(lock_do_i_hold(lock)));
+
+        KASSERT(curthread->t_in_interrupt == false);
+
+#if USE_SEMAPHORE_FOR_LOCK
+/*
+ *  G.Cabodi - 2019: P BEFORE(!!!) spinlock acquire. OS161 forbids sleeping/realeasing
+ *  the CPU while owning a spinlocks: this could be a cause of deadlock. 
+ *  THE spinlock passed to wchan_wait is the only one allowed. 
+ *  This is checked in various parts of the code (see for instance wchan_sleep.
+ *  as P may result in "wait", it cannot be called while owning the spinlock.
+ */
+        P(lock->lk_sem);
+	spinlock_acquire(&lock->lk_lock);        
+#else
+	spinlock_acquire(&lock->lk_lock);        
+	while (lock->lk_owner != NULL) {
+	  wchan_sleep(lock->lk_wchan, &lock->lk_lock);
+        }
+#endif
+        KASSERT(lock->lk_owner == NULL);
+        lock->lk_owner=curthread;
+	spinlock_release(&lock->lk_lock);
+#endif
+        (void)lock;  // suppress warning until code gets written
+}
+
+void
+lock_release(struct lock *lock)
+{
+        // Write this
+#if OPT_SYNCH
+	KASSERT(lock != NULL);
+	KASSERT(lock_do_i_hold(lock));
+	spinlock_acquire(&lock->lk_lock);
+        lock->lk_owner=NULL;
+	/*  G.Cabodi - 2019: no problem here owning a spinlock, as V/wchan_wakeone 
+	    do not lead to wait state */
+#if USE_SEMAPHORE_FOR_LOCK
+        V(lock->lk_sem);
+#else
+        wchan_wakeone(lock->lk_wchan, &lock->lk_lock);
+#endif
+	spinlock_release(&lock->lk_lock);
+#endif
+
+        (void)lock;  // suppress warning until code gets written
+}
+
+bool
+lock_do_i_hold(struct lock *lock)
+{
+        // Write this
+#if OPT_SYNCH
+        bool res;
+	/*  G.Cabodi - 2019: this could possibly work without spinlock for mutual 
+	    exclusion, which could simplify the semaphore-based solution, by 
+	    removing the spinlock. 
+	    Whenever the current thread owns the lock, the test is safe without 
+	    guaranteeing mutual exclusion. 
+	    If NOT the owner, a wrong verdict could happen (very low chance!!!)
+            by wrongly reading a pointer == curthread. However, using the spinlock 
+	    is good practice for shared data. */
+	spinlock_acquire(&lock->lk_lock);
+	res = lock->lk_owner == curthread;
+	spinlock_release(&lock->lk_lock);
+	return res;
+#endif
+
+        (void)lock;  // suppress warning until code gets written
+
+        return true; // dummy until code gets written
+}
+
+////////////////////////////////////////////////////////////
+//
+// CV
+
+
+struct cv *
+cv_create(const char *name)
+{
+        struct cv *cv;
+
+        cv = kmalloc(sizeof(*cv));
+        if (cv == NULL) {
+                return NULL;
+        }
+
+        cv->cv_name = kstrdup(name);
+        if (cv->cv_name==NULL) {
+                kfree(cv);
+                return NULL;
+        }
+
+        // add stuff here as needed
+#if OPT_SYNCH
+	cv->cv_wchan = wchan_create(cv->cv_name);
+	if (cv->cv_wchan == NULL) {
+	        kfree(cv->cv_name);
+		kfree(cv);
+		return NULL;
+	}
+        spinlock_init(&cv->cv_lock);
+#endif
+        return cv;
+}
+
+void
+cv_destroy(struct cv *cv)
+{
+        KASSERT(cv != NULL);
+
+        // add stuff here as needed
+#if OPT_SYNCH
+	spinlock_cleanup(&cv->cv_lock);
+	wchan_destroy(cv->cv_wchan);
+#endif
+        kfree(cv->cv_name);
+        kfree(cv);
+}
+
+void
+cv_wait(struct cv *cv, struct lock *lock)
+{
+        // Write this
+#if OPT_SYNCH
+        KASSERT(lock != NULL);
+	KASSERT(cv != NULL);
+	KASSERT(lock_do_i_hold(lock));
+
+	spinlock_acquire(&cv->cv_lock);
+	/* G.Cabodi - 2019: spinlock already owned as atomic lock_release+wchan_sleep
+	   needed */
+	lock_release(lock);
+	wchan_sleep(cv->cv_wchan,&cv->cv_lock);
+	spinlock_release(&cv->cv_lock);
+	/* G.Cabodi - 2019: spinlock already  released to avoid ownership while
+	   (possibly) going to wait state in lock_acquire. 
+	   Atomicity wakeup+lock_acquire not guaranteed (but not necessary!) */
+	lock_acquire(lock);
+#endif
+
+        (void)cv;    // suppress warning until code gets written
+        (void)lock;  // suppress warning until code gets written
+}
+
+void
+cv_signal(struct cv *cv, struct lock *lock)
+{
+        // Write this
+#if OPT_SYNCH
+        KASSERT(lock != NULL);
+	KASSERT(cv != NULL);
+	KASSERT(lock_do_i_hold(lock));
+	/* g.Cabodi - 2019: here the spinlock is NOT required, as no atomic operation 
+	   has to be done. The spinlock is just acquired because needed by wakeone */
+	spinlock_acquire(&cv->cv_lock);
+	wchan_wakeone(cv->cv_wchan,&cv->cv_lock);
+	spinlock_release(&cv->cv_lock);
+#endif
+	(void)cv;    // suppress warning until code gets written
+	(void)lock;  // suppress warning until code gets written
+}
+
+void
+cv_broadcast(struct cv *cv, struct lock *lock)
+{
+	// Write this
+#if OPT_SYNCH
+        KASSERT(lock != NULL);
+	KASSERT(cv != NULL);
+	KASSERT(lock_do_i_hold(lock));
+	/* G.Cabodi - 2019: see comment on spinlocks in cv_signal */
+	spinlock_acquire(&cv->cv_lock);
+	wchan_wakeall(cv->cv_wchan,&cv->cv_lock);
+	spinlock_release(&cv->cv_lock);
+#endif
+	(void)cv;    // suppress warning until code gets written
+	(void)lock;  // suppress warning until code gets written
+}
